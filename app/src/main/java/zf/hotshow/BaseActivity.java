3fb8b15e4befeed0822a8840757a70af78175edf
qq602678827 <602678827@qq.com>
2016-12-21 21:14:12 +0800

    1

--- app/src/main/java/zf/hotshow/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package zf.hotshow;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Administrator on 2016/12/21.
- */
-public class BaseActivity  extends AppCompatActivity {
-}
